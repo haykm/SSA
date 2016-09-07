fb4fe79f3892e3775491dade66e05628e608bbdf
haykm <mamikonyanh@gmail.com>
2016-09-08 00:23:53 +0400

    bootstrap nav_bar plus some logo. V2

--- nav_bar.php
+++ nav_bar.php
@@ -6,14 +6,16 @@
   <title>Bootstrap Case</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="style.css">
   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body>
-    <div class="logo">  
-        <img src="resource/img/logo1.jpg" height="100px">
-        <img src="./resource/img/big_logo1.png" ></div>
+<div id="logo">  
+    <img src="./resource/img/logo1.jpg" >
+    <img src="./resource/img/big_logo1.png" >
+</div>
 <div class="main">    
     <nav class="navbar navbar-inverse">
       <div class="container-fluid">
