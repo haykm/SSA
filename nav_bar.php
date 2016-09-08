f98cc88119459dafdb6331fe5a3f95a725711b19
haykm <mamikonyanh@gmail.com>
2016-09-09 00:15:33 +0400

    some changes in nav bar

--- nav_bar.php
+++ nav_bar.php
@@ -3,18 +3,19 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Bootstrap Case</title>
+  <title>SSA</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="style.css">
   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="style.css">
 </head>
 <body>
 <div id="logo">  
     <img src="./resource/img/logo1.jpg" >
-    <img src="./resource/img/big_logo1.png" >
+    
+    <img src="./resource/img/big_logo1.png" id="long_logo">
 </div>
 <div class="main">    
     <nav class="navbar navbar-inverse">
@@ -32,7 +33,7 @@
         <div class="collapse navbar-collapse" id="myNavbar">
           <ul class="nav navbar-nav">
             <li class="active"><a href="#">Home</a></li>
-            <li class="dropdown">
+<!--            <li class="dropdown">
               <a class="dropdown-toggle" data-toggle="dropdown" href="#">Page 1 <span class="caret"></span></a>
               <ul class="dropdown-menu">
                 <li><a href="#">Page 1-1</a></li>
@@ -40,8 +41,20 @@
                 <li><a href="#">Page 1-3</a></li>
               </ul>
             </li>
-            <li><a href="#">Page 2</a></li>
-            <li><a href="#">Page 3</a></li>
+            -->
+            <li><a href="#">Football</a></li>
+            <li><a href="#">Basketball</a></li>
+            <li><a href="#">Volleyball</a></li>
+            <li><a href="#">Squash</a></li>
+            <li class="dropdown">
+              <a class="dropdown-toggle" data-toggle="dropdown" href="#">Tournaments <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">Football</a></li>
+                <li><a href="#">Basketball</a></li>
+                <li><a href="#">Volleyball</a></li>
+                <li><a href="#">Squash</a></li>
+              </ul>
+            </li>
           </ul>
           <ul class="nav navbar-nav navbar-right">
             <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
