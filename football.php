70c59741594195362a8494ff3b13d5b77e863866
haykm <mamikonyanh@gmail.com>
2016-09-29 00:10:06 +0400

    switched to players table

--- football.php
+++ football.php
@@ -10,8 +10,23 @@
 // else {
 //    echo 'bbb';    
 //}
-    $query = "SELECT * FROM `team`";
+    $query = "SELECT * FROM team";
+    $query1 = "SELECT DISTINCT team_name FROM players";
     $row = mysqli_query($db, $query);
+    $team_list_obj=mysqli_query($db, $query1);
+    $i=0;
+    $team_array= array();
+    while ($team_list=  mysqli_fetch_assoc($team_list_obj)){
+        $team_array[]=$team_list['team_name'];
+//        echo $team_list['team_name'];
+    }
+    print_r($team_array);
+    ?>
+
+    
+//print_r($team_array);
+//    print_r($team_list);
+//    print_r($row);
 //    mysqli_query($link, $query)
     
 ?>
@@ -33,20 +48,20 @@
 
         <!--<a href="#">-->
             <div class="teams col-lg-3 col-md-3 col-sx-4 bordered1">
-                <table class="table-bordered table table-condensed table-responsive">
+                <table class="table-bordered table table-condensed ">
                     <thead>
                         <tr>
                             <th>TeamName</th>
                             <th>CaptainName</th>
-                            <th alt="Number">N</th>
+                            <th title="Number Of Players" >N</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <?php while ($team_list = mysqli_fetch_assoc($row)): ?>
+                        <?php while ($teams = mysqli_fetch_assoc($row)): ?>
                         <tr>
-                           <td><?php echo $team_list['team_name'] ?></td>
-                           <td><?= $team_list['captain_name'] ?></td>
-                           <td><?= $team_list['num'] ?> </td>
+                            <td><a href="#"><?php echo $teams['team_name'] ?></a></td>
+                           <td><?= $teams['captain_name'] ?></td>
+                           <td><?= $teams['num'] ?> </td>
                        </tr> 
                        <?php endwhile; ?>
                     </tbody>
@@ -59,8 +74,35 @@
         </div>
         <a  href="#">
             <div class="registr col-lg-3 col-md-3 col-sx-4 bordered1">
-                register your team
-            </div>
+                <table class="table-bordered table table-condensed ">
+                    <thead>
+                        <tr>
+                            <th>TeamName</th>
+                            <th>CaptainName</th>
+                            <th title="Number Of Players" >N</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                            foreach( $team_array as $team){
+                                $query2="SELECT * FROM players WHERE team_name='$team'";
+                                $row1= mysqli_query($db, $query2);
+//                                while($team_player=mysqli_fetch_assoc($row1)){
+//                                    echo $team_player['l_name']; echo '<br>';
+//                                }
+                                 $qq=mysqli_fetch_assoc($row1);
+                                echo "<tr> "
+                                . "<td><a href='#'>" . $team . "</a></td>"
+                                    . "<td>" . $qq['catain_name']
+                                    . "</td>"
+                                    . "<td>" . mysqli_num_rows($row1) . "</td>"
+                                    . "</tr>";
+//                                echo mysqli_num_rows($row1);
+                            }
+                        ?>
+                    </tbody>
+                </table>
+           </div>
         </a>
             <div class=" col-lg-1 col-md-1 ">
         </div>
