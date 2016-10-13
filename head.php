b15ab393048e22eb94f794776b38e0361871b1d4
haykm <mamikonyanh@gmail.com>
2016-10-13 22:31:38 +0400

    adding modal for login: step2

--- head.php
+++ head.php
@@ -9,6 +9,8 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="form.css">
+  
 </head>
 <body>
 
--- login.php
+++ login.php
@@ -7,21 +7,38 @@
     <div class="modal-dialog">
     
       <!-- Modal content-->
-      <div class="modal-content">
-        <div class="modal-header">
+      <!--<div class="modal-content">-->
+<!--        <div class="modal-header">
           <button class="close" type="button" onclick="close_modal();" aria-label="Close" >&times;</button>
           <h2 class="modal-title"> LogIn</h2>
+        </div>-->
+<!--        <div class="modal-body">-->
+        <div class="container">
+            <section id="content">
+                <button class="close" type="button" onclick="close_modal();" aria-label="Close" >&times;</button>
+                <form action="">
+                    
+                        <h1>Login Form</h1>
+                        
+                        <div>
+                                <input type="text" placeholder="Username" required="" id="username" />
+                        </div>
+                        <div>
+                                <input type="password" placeholder="Password" required="" id="password" />
+                        </div>
+                        <div>
+                                <input type="submit" value="Log in" />
+                                <a href="#">Lost your password?</a>
+                                <a href="#">Register</a>
+                        </div>
+                </form><!-- form -->
+            </section>
         </div>
-        <div class="modal-body">
-            <form>
-                <input type="text" >
-                <input type="password">
-            </form>
-        </div>
-        <div class="modal-footer">
+        <!--</div>-->
+<!--        <div class="modal-footer">
           <button type="button" class="btn btn-default" onclick="close_modal();">Close</button>
-        </div>
-      </div>
+        </div>-->
+      <!--</div>-->
       
     </div>
   </div>
