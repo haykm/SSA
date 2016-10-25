47cf14afd22d729d850133679eb3b12620b0752e
haykm <mamikonyanh@gmail.com>
2016-10-26 00:29:30 +0400

    adding some functionality for registrated users.

--- football.php
+++ football.php
@@ -10,18 +10,26 @@
 // else {
 //    echo 'bbb';    
 //}
-    $query = "SELECT * FROM team";
+    $query = "SELECT *  FROM team";
     $query1 = "SELECT DISTINCT team_name FROM players";
+    $capts_query = "SELECT DISTINCT capt_uname FROM players";
     $row = mysqli_query($db, $query);
     $team_list_obj=mysqli_query($db, $query1);
+    $capts_obj=mysqli_query($db,$capts_query );
     $i=0;
     $team_array= array();
+    $capt_list= array();
     while ($team_list=  mysqli_fetch_assoc($team_list_obj)){
         $team_array[]=$team_list['team_name'];
 //        echo $team_list['team_name'];
     }
+
 //    print_r($team_array);
-    echo "user is ".$_SESSION['user'];
+//    echo "user is ".$_SESSION['user'];
+    if(isset($_SESSION['user'])){$capt=$_SESSION['user'];} else {$capt='no_user';}
+    while ($c=mysqli_fetch_assoc($capts_obj)){
+        if($c==$capt){$user=2;break;}  else {$user=1;}
+    }
     ?>
 
     
@@ -62,15 +70,20 @@
                         <?php foreach( $team_array as $team){
                             $query2="SELECT * FROM players WHERE team_name='$team'";
                             $row1= mysqli_query($db, $query2); 
-                            $qq=mysqli_fetch_assoc($row1);
+                            $qq=mysqli_fetch_assoc($row1); // xaxacoxi tox@ "players" table-um
                             ?>
                         <tr>
                             <td><a data-toggle="modal" data-keyboard="true" href="#" 
                                    onclick="teamModal1('<?php echo $team ?>')"><?php echo $team ?>
                                 </a>
-                                <button class="btn btn-xs btn-success" 
-                                   onclick="teamModal1('<?php echo $team ?>')">Details 
-                                </button>
+                                
+                                <?php 
+                                if($qq['capt_uname']===$capt){
+                                    echo '<button class="btn btn-xs btn-success" onclick="teamModal1()">'. 
+                                    "EDIT".'</button>';
+                                }
+                                ?>
+                                
                             </td>
                            <td><?php echo $qq['catain_name']  ?></td>
                            <td><?php echo mysqli_num_rows($row1) ?> </td>
@@ -87,7 +100,19 @@
     </div>
         
         <div class="registr col-lg-3 col-md-3 col-sx-4 bordered1">
-            register your team
+            <?php 
+                
+                if($capt!='no_user'){
+                    if($user==2){
+                        echo 'you have already registrated team. Each patain could register only one team for each type of sport';
+                    }else{echo 'Now You can register your team. '.$user;}
+                }else {echo
+                    "<p> Only captains can register a team. <br> If you are a captain, please log in as with your username to enable additional functions.
+                    </p>
+                    <p>Team should have at least 5 players, each of which is not included in other teams</p>
+                    <p>If you don't have account yet, you can SignUp right now.</p>"
+                ;}
+            ?>
 
        </div>
         
